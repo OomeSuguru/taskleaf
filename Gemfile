dd9f4d68ae31219697f0a4a1a89237069155ccbd
OomeSuguru <sususu0717@gmail.com>
2018-11-19 16:03:02 +0900

    save

--- Gemfile
+++ Gemfile
@@ -25,7 +25,7 @@ gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
@@ -48,6 +48,11 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'pry-rails'  # rails console(もしくは、rails c)でirbの代わりにpryを使われる
+  gem 'pry-doc'    # methodを表示
+  gem 'pry-byebug' # デバッグを実施(Ruby 2.0以降で動作する)
+  gem 'pry-stack_explorer' # スタックをたどれる
+  gem 'factory_bot_rails', '~> 4.11'
 end
 
 group :test do
@@ -56,6 +61,7 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
+  gem 'rspec-rails', '~> 3.7'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
@@ -63,3 +69,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'slim-rails'
 gem 'html2slim'
 gem 'bootstrap'
+gem 'rails_autolink'
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,41 +1,49 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def new
     @task = Task.new
   end
 
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks
+    # .order(created_at: :desc)
+    # @tasks = Task.all
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    task = Task.find(params[:id])
-    task.update!(task_params)
-    redirect_to tasks_url, notice: "タスク「#{task.name}を更新しました」"
+    @task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{@task.name}を更新しました」"
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def create
-    task = Task.new(task_params)
-    task.save!
-    redirect_to tasks_url　notice: "タスク「#{task.name}」を登録しました"
+    @task = current_user.tasks.new(task_params)
+    if @task.save!
+      redirect_to tasks_url, notice: "タスク「#{@task.name}」を作成しました"
+    else
+      render :new
+    end
   end
 
   def destroy
-    task = Task.find(params[:id])
-    task.destroy
-    redirect_to tasks_url, notice: "タスク「#{task.name}を削除しました」"
+    @task.destroy
+    redirect_to tasks_url, notice: "タスク「#{@task.name}を削除しました」"
   end
+
+
   private
 
   def task_params
     params.require(:task).permit(:name, :description)
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
   end
 end
